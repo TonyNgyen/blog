241a865c17b163fc4d3b8a6a34dec2b642eca7ed
Tony Nguyen <tonynguyeenn@gmail.com>
2023-12-31 18:48:44 -0800

    Preparing for production

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      "mongodb-client-encryption": false ,
+      "aws4": false
+    };
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
