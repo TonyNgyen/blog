78b8a57c02610b73649bc60b694fd30dad8abf3e
Tony Nguyen <tonynguyeenn@gmail.com>
2024-01-01 04:12:44 -0800

    Configuring MongoDB settings to deploy onto vercel

--- next.config.js
+++ next.config.js
@@ -8,14 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      "mongodb-client-encryption": false ,
-      "aws4": false
-    };
-
-    return config;
-  }
 };
 
 module.exports = nextConfig;
