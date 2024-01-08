be58cdf141a5c1fe78c1fa47de29fcd7c68de174
Tony Nguyen <tonynguyeenn@gmail.com>
2024-01-07 23:58:42 -0800

    Fixing Vercel Deployment

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export const connectToDb = async () => {
         console.log("Using exists connection")
       return
     }
-    const db = await mongoose.connect("mongodb+srv://Tony:XM8tJkFVrT3IIx4A@cluster0.yfbxcio.mongodb.net/Blog?retryWrites=true&w=majority");
+    const db = await mongoose.connect(process.env.MONGODB_URI);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
